7a12fb234e9586e4df0d2f0e93ab3d38bf79e04d
mathieu bouchant <matbcht@gmail.com>
2019-09-24 14:51:38 +0200

    last maj

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'new', to: 'games#new', as: :new
+  get 'score', to: 'games#score', as: :score
+
+  get 'games/score'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
